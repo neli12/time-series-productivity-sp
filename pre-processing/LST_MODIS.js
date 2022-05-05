c6c85a5e0dc33583d1079440e0691013decabf9a
Neli <neliquinhonez92@gmail.com>
2022-05-05 08:00:37 -0300

    Update LST_MODIS.js

--- pre-processing/LST_MODIS.js
+++ pre-processing/LST_MODIS.js
@@ -1,6 +1,6 @@
 //Load and filter collection
 var LST = ee.ImageCollection('MODIS/061/MOD11A1')
-                  .filter(ee.Filter.date('2016-01-01', '2020-12-01'))
+                  .filter(ee.Filter.date('2000-01-01', '2020-12-31'))
                   .filterBounds(geometry)
                   .select('LST_Day_1km');
 print(LST);
@@ -18,20 +18,15 @@ var LST_vis = {min: 13000.0, max: 16500.0, palette: [
 Map.addLayer(LST, LST_vis, 'Land Surface Temperature');
     
 //Create two list for months and years
-var months = ee.List.sequence(1, 12);
 var years = ee.List.sequence(2016, 2020);
 
 // Map filtering and reducing across year-month combinations and convert to ImageCollection
 var yrMo = ee.ImageCollection.fromImages(
   years.map(function (y) {
-        return months.map(function (m) {
             return LST
               .filter(ee.Filter.calendarRange(y, y, 'year'))
-              .filter(ee.Filter.calendarRange(m, m, 'month'))
               .mean()
               .set('year',y)
-              .set('month',m);
-        });
     }).flatten());
 print("yrMo",yrMo);
 
@@ -43,6 +38,6 @@ print(LST_clip);
 
 //Export image to Drive
 Export.image.toDrive({image: LST_clip,
-                      description: 'LST_2016-2020',
+                      description: 'LST_2000-2001',
                       region: geometry,
                       crs: 'EPSG:4326'})
